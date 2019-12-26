b269bfc8cded3c5417a179a4677e2891bd56e5e9
sai pavan <pavanammula@gmail.com>
2019-12-26 22:54:58 +0530

    Autocomplete in JS

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const search = document.getElementById('search');
+const matchList = document.getElementById('match-list');
+const searchBooks = async searchText => {
+    const res = await fetch('../data/books.json');
+    const books = await res.json();
+    let matches = books.filter(book => {
+        const regex = new RegExp(`^${searchText}`,'gi');
+        return book.title.match(regex) || book.author.match(regex);
+    })
+    console.log(matches);
+    if (searchText.length === 0) {
+        matches = [];
+    }
+    outputHtml(matches);
+}
+const outputHtml = matches => {
+    if (matches.length > 0) {
+        const html = matches
+            .map(
+                match => `
+                <div class="card card-body mb-1">
+                    <h4>${match.title} (${match.author}) <span
+                    class="text-primary">
+                    ${match.year}</h4>
+                    <small>country: ${match.country} / pages:${match.pages}</small>
+                </div>
+                `
+            )
+            .join('');
+        matchList.innerHTML = html;
+                
+    } else {
+        matchList.innerHTML = [];
+    }
+}
+search.addEventListener('input',() => searchBooks(search.value))   
